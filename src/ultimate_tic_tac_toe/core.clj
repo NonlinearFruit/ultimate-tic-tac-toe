b1b4281b33af149e33fad8d74ec02d3d63dd30f6
NonlinearFruit <1123benji5813@gmail.com>
2025-01-04 22:05:08 -0700

    Playing with prints and reads

--- src/ultimate_tic_tac_toe/core.clj
+++ src/ultimate_tic_tac_toe/core.clj
@@ -1,9 +1,5 @@
 (ns ultimate-tic-tac-toe.core)
 
-(defn -main
-  [& args]
-  (println "Hello World"))
-
 (def lines {
   :rows [[0 1 2] [3 4 5] [6 7 8]]
   :columns [[0 3 6] [1 4 7] [2 5 8]]
@@ -13,3 +9,31 @@
   (not (empty? (filter
     (fn [line] (= #{symbol} (into #{} (map #(board %1) line))))
     (apply concat (vals lines))))))
+
+(defn print-title []
+  (println 
+    (clojure.string/replace
+      (clojure.string/join "\n" [
+        "           _   _ _ _   _                 _                "
+        "          | | | | | |_(_)_ __ ___   __ _| |_ ___          "
+        "          | | | | | __| | '_ ` _ ? / _` | __/ _ ?         "
+        "          | |_| | | |_| | | | | | | (_| | ||  __/         "
+        "           ?___/|_|?__|_|_| |_| |_|?__,_|?__?___|         "
+        "      _____ _        _____            _____               "
+        "     |_   _(_) ___  |_   _|_ _  ___  |_   _|__   __       "
+        "       | | | |/ __|   | |/ _` |/ __|   | |/ _ ? / _ ?     "
+        "       | | | | (__    | | (_| | (__    | | (_) |  __/     "
+        "       |_| |_|?___|   |_|?__,_|?___|   |_|?___/ ?___|     "
+        "                                                          "])
+      "?" "\\")))
+
+(defn -main
+  [& args]
+  (print-title)
+  (print "Board: ")
+  (flush)
+  (println "hmm... " (read-line))
+  (print "Square: ")
+  (flush)
+  (println "hmm... " (read-line)))
+
