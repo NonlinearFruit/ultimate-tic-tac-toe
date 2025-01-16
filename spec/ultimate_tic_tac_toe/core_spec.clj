b5bca603cd4d449829b9519155d82305879e6ac5
NonlinearFruit <1123benji5813@gmail.com>
2025-01-15 21:38:05 -0700

    Ability to print the board

--- spec/ultimate_tic_tac_toe/core_spec.clj
+++ spec/ultimate_tic_tac_toe/core_spec.clj
@@ -2,29 +2,80 @@
   (:require [speclj.core :refer :all]
             [ultimate-tic-tac-toe.core :refer :all]))
 
-(describe "Has player won?"
-  (it "has no win for nil board"
-    (should= false (has-player-won? :x [nil nil nil nil nil nil nil nil nil])))
-  (it "has no win for cats game"
-    (should= false (has-player-won? :x [:x :o :x :x :o :x :o :x :o])))
-  (it "has no win for opponents game"
-    (should= false (has-player-won? :x [:o :o :o nil nil nil nil nil nil])))
+(describe "_ultimate_ tic tac toe"
+  (context "has player won?"
+    (it "has no win for nil board"
+      (should= false (has-player-won? :x [nil nil nil nil nil nil nil nil nil])))
+    (it "has no win for cats game"
+      (should= false (has-player-won? :x [:x :o :x :x :o :x :o :x :o])))
+    (it "has no win for opponents game"
+      (should= false (has-player-won? :x [:o :o :o nil nil nil nil nil nil])))
 
-  (it "finds top row win"
-    (should= true (has-player-won? :x [:x :x :x nil nil nil nil nil nil])))
-  (it "finds middle row win"
-    (should= true (has-player-won? :x [nil nil nil :x :x :x nil nil nil])))
-  (it "finds bottom row win"
-    (should= true (has-player-won? :x [nil nil nil nil nil nil :x :x :x])))
+    (it "finds top row win"
+      (should= true (has-player-won? :x [:x :x :x nil nil nil nil nil nil])))
+    (it "finds middle row win"
+      (should= true (has-player-won? :x [nil nil nil :x :x :x nil nil nil])))
+    (it "finds bottom row win"
+      (should= true (has-player-won? :x [nil nil nil nil nil nil :x :x :x])))
 
-  (it "finds first column win"
-    (should= true (has-player-won? :x [:x nil nil :x nil nil :x nil nil])))
-  (it "finds middle column win"
-    (should= true (has-player-won? :x [nil :x nil nil :x nil nil :x nil])))
-  (it "finds last column win"
-    (should= true (has-player-won? :x [nil nil :x nil nil :x nil nil :x])))
+    (it "finds first column win"
+      (should= true (has-player-won? :x [:x nil nil :x nil nil :x nil nil])))
+    (it "finds middle column win"
+      (should= true (has-player-won? :x [nil :x nil nil :x nil nil :x nil])))
+    (it "finds last column win"
+      (should= true (has-player-won? :x [nil nil :x nil nil :x nil nil :x])))
 
-  (it "finds forward diagonal win"
-    (should= true (has-player-won? :x [nil nil :x nil :x nil :x nil nil])))
-  (it "finds backward diagonal win"
-    (should= true (has-player-won? :x [:x nil nil nil :x nil nil nil :x]))))
+    (it "finds forward diagonal win"
+      (should= true (has-player-won? :x [nil nil :x nil :x nil :x nil nil])))
+    (it "finds backward diagonal win"
+      (should= true (has-player-won? :x [:x nil nil nil :x nil nil nil :x]))))
+
+  (context "stringy board"
+    (it "makes a blank board"
+      (should= " | | \n-+-+-\n | | \n-+-+-\n | | " (stringify-board [nil nil nil nil nil nil nil nil nil])))
+    (it "populates each cell dynamically"
+      (should= "a|b|c\n-+-+-\nd|e|f\n-+-+-\ng|h|i" (stringify-board [:a :b :c :d :e :f :g :h :i]))))
+
+  ; (context "stringy multiboard"
+  ;   (it "makes a blank multiboard"
+  ;     (should=  "         |         |         \n   | |   |   | |   |   | |   \n  -+-+-  |  -+-+-  |  -+-+-  \n   | |   |   | |   |   | |   \n  -+-+-  |  -+-+-  |  -+-+-  \n   | |   |   | |   |   | |   \n         |         |         \n---------+---------+---------\n         |         |         \n   | |   |   | |   |   | |   \n  -+-+-  |  -+-+-  |  -+-+-  \n   | |   |   | |   |   | |   \n  -+-+-  |  -+-+-  |  -+-+-  \n   | |   |   | |   |   | |   \n         |         |         \n---------+---------+---------\n         |         |         \n   | |   |   | |   |   | |   \n  -+-+-  |  -+-+-  |  -+-+-  \n   | |   |   | |   |   | |   \n  -+-+-  |  -+-+-  |  -+-+-  \n   | |   |   | |   |   | |   \n         |         |         " (stringify-multi-board [
+  ;       [nil nil nil nil nil nil nil nil nil]
+  ;       [nil nil nil nil nil nil nil nil nil]
+  ;       [nil nil nil nil nil nil nil nil nil]
+  ;       [nil nil nil nil nil nil nil nil nil]
+  ;       [nil nil nil nil nil nil nil nil nil]
+  ;       [nil nil nil nil nil nil nil nil nil]
+  ;       [nil nil nil nil nil nil nil nil nil]
+  ;       [nil nil nil nil nil nil nil nil nil]
+  ;       [nil nil nil nil nil nil nil nil nil]])))
+  ;   (it "makes a realistic multiboard"
+  ;     (should= "" (stringify-multi-board [
+  ;       [:x  :o  :x  :x  :o  :x  :o  :x  :o]
+  ;       [:x  :o  :x  :x  :o  :x  :o  :x  :o]
+  ;       [:x  :o  :x  :x  :o  :x  :o  :x  :o]
+  ;       [nil nil nil nil nil nil nil nil nil]
+  ;       [:x  :o  :x  :x  :o  :x  :o  :x  :o]
+  ;       [:x  :o  :x  :x  :o  :x  :o  :x  :o]
+  ;       [:x  :o  :x  :x  :o  :x  :o  :x  :o]
+  ;       [nil nil nil nil nil nil nil nil nil]
+  ;       [:x  :o  :x  :x  :o  :x  :o  :x  :o]
+  ;       [:x  :o  :x  :x  :o  :x  :o  :x  :o]
+  ;       [:x  :o  :x  :x  :o  :x  :o  :x  :o]
+  ;       [nil nil nil nil nil nil nil nil nil]]))))
+          
+  (context "split multiboard into layers"
+    (it "turns a single board into 5 layers"
+      (should= 5 (count (split-into-layers [[nil nil nil nil nil nil nil nil nil]]))))
+    (it "turns two boards into 10 layers"
+      (should= 10 (count (split-into-layers [[nil nil nil nil nil nil nil nil nil] [nil nil nil nil nil nil nil nil nil]])))))
+          
+  (context "order layers for inserting"
+    (it "puts layers in correct order"
+      (should= (range 0 45) (order-layers-for-inserting [
+         0  3  6  9 12   1  4  7 10 13   2  5  8 11 14
+        15 18 21 24 27  16 19 22 25 28  17 20 23 26 29
+        30 33 36 39 42  31 34 37 40 43  32 35 38 41 44])))
+    (it "moves the 5th layer to the 13th spot"
+      (should= :tracer (nth (order-layers-for-inserting [nil nil nil nil :tracer nil nil nil nil nil nil nil nil nil nil]) 12)))))
+
+(run-specs)
--- src/ultimate_tic_tac_toe/core.clj
+++ src/ultimate_tic_tac_toe/core.clj
@@ -10,6 +10,121 @@
     (fn [line] (= #{symbol} (into #{} (map #(board %1) line))))
     (apply concat (vals lines))))))
 
+(defn stringify-board [board]
+  (apply format
+    "%s|%s|%s\n-+-+-\n%s|%s|%s\n-+-+-\n%s|%s|%s" 
+    (map #(if (some? %1) (name %1) " ") (flatten board))))
+
+(defn split-into-layers [multiboard]
+  (flatten (map
+    #(clojure.string/split (stringify-board %1) #"\n")
+    multiboard)))
+
+(defn order-layers-for-inserting [layers]
+  (map last
+    (sort-by
+      #(let [i (first %1)]
+         (+
+           (mod (* 3 i) 15)
+           (mod (quot i 5) 3)
+           (* (quot i 15) 15)))
+      (map-indexed (fn [i e] [i e]) layers))))
+
+(defn stringify-multi-board [multiboard]
+  (apply format
+    (clojure.string/replace
+      (clojure.string/join "\n" [
+        "         |         |         "
+        "  layer  |  layer  |  layer  "
+        "  layer  |  layer  |  layer  "
+        "  layer  |  layer  |  layer  "
+        "  layer  |  layer  |  layer  "
+        "  layer  |  layer  |  layer  "
+        "         |         |         "
+        "---------+---------+---------"
+        "         |         |         "
+        "  layer  |  layer  |  layer  "
+        "  layer  |  layer  |  layer  "
+        "  layer  |  layer  |  layer  "
+        "  layer  |  layer  |  layer  "
+        "  layer  |  layer  |  layer  "
+        "         |         |         "
+        "---------+---------+---------"
+        "         |         |         "
+        "  layer  |  layer  |  layer  "
+        "  layer  |  layer  |  layer  "
+        "  layer  |  layer  |  layer  "
+        "  layer  |  layer  |  layer  "
+        "  layer  |  layer  |  layer  "
+        "         |         |         "])
+      "layer", "%s")
+    (order-layers-for-inserting (split-into-layers multiboard))))
+
+(defn print-multi-board [multiboard]
+  (println 
+      (clojure.string/join "\n" [
+        "                                      "
+        "                                      "
+        "                                      "
+        "                  |         |         "
+        "            | |   |  1|2|3  |   | |   "
+        "           -+-+-  |  -+-+-  |  -+-+-  "
+        "            | |   |  4|5|6  |   | |   "
+        "           -+-+-  |  -+-+-  |  -+-+-  "
+        "            | |   |  7|8|9  |   | |   "
+        "                  |         |         "
+        "         ---------+---------+---------"
+        "                  |         |         "
+        "           X|X|O  |   | |   |   | |   "
+        "           -+-+-  |  -+-+-  |  -+-+-  "
+        "           O|X|X  |  O|X|   |   | |   "
+        "           -+-+-  |  -+-+-  |  -+-+-  "
+        "           O|O|X  |   | |   |   | |   "
+        "                  |         |         "
+        "         ---------+---------+---------"
+        "                  |         |         "
+        "           X   X  |  OOOOO  |   | |   "
+        "            X X   |  O   O  |  -+-+-  "
+        "             X    |  O   O  |   | |   "
+        "            X X   |  O   O  |  -+-+-  "
+        "           X   X  |  OOOOO  |   | |   "
+        "                  |         |         "
+        "                                      "])))
+
+(defn print-board-selection []
+  (println 
+      (clojure.string/join "\n" [
+        "                                      "
+        "                                      "
+        "                                      "
+        "                  |         |         "
+        "             1    |   222   |  3333   "
+        "             1    |  2   2  |      3  "
+        "             1    |     2   |   333   "
+        "             1    |    2    |      3  "
+        "             1    |   2222  |  3333   "
+        "                  |         |         "
+        "         ---------+---------+---------"
+        "                  |         |         "
+        "              4   |   5555  |  66666  "
+        "           4  4   |  5      |  6      "
+        "           4444   |  55555  |  66666  "
+        "              4   |      5  |  6   6  "
+        "              4   |  55555  |  66666  "
+        "                  |         |         "
+        "         ---------+---------+---------"
+        "                  |         |         "
+        "           77777  |   888   |  99999  "
+        "               7  |  8   8  |  9   9  "
+        "              7   |   888   |  99999  "
+        "             7    |  8   8  |      9  "
+        "            7     |   888   |  99999  "
+        "                  |         |         "
+        "                                      "])))
+
+(defn clear-screen []
+  (print "\033[H\033[2J"))
+
 (defn print-title []
   (println 
     (clojure.string/replace
@@ -30,10 +145,24 @@
 (defn -main
   [& args]
   (print-title)
+  (println "                <<Press enter to start>>")
+  (read-line)
+  (clear-screen)
+  (print-board-selection)
   (print "Board: ")
   (flush)
   (println "hmm... " (read-line))
   (print "Square: ")
   (flush)
-  (println "hmm... " (read-line)))
+  (println "hmm... " (read-line))
+  (println (stringify-multi-board [
+        [nil nil nil nil nil nil nil nil nil]
+        [nil nil nil nil nil nil nil nil nil]
+        [nil nil nil nil nil nil nil nil nil]
+        [nil nil nil nil nil nil nil nil nil]
+        [nil nil nil nil nil nil nil nil nil]
+        [nil nil nil nil nil nil nil nil nil]
+        [nil nil nil nil nil nil nil nil nil]
+        [nil nil nil nil nil nil nil nil nil]
+        [nil nil nil nil nil nil nil nil nil]])))
 
