19dc3e52bb0541f848d9050655e927bf3ddf63b5
NonlinearFruit <1123benji5813@gmail.com>
2025-01-30 20:24:29 -0700

    Organize functions a little better

--- src/ultimate_tic_tac_toe/core.clj
+++ src/ultimate_tic_tac_toe/core.clj
@@ -26,13 +26,19 @@
 (defn opponent [symbol]
   (if (= :x symbol) :o :x))
 
-(defn has-cat-won? [board]
-  (not (some? (some nil? board))))
-
 (defn has-player-won? [symbol board]
   (some? (some
            (fn [line] (every? #(= symbol %1) (map board line)))
            lines)))
+
+(defn has-cat-won? [board]
+  (not (some? (some nil? board))))
+
+(defn is-complete? [board]
+  (or
+    (has-player-won? :x board)
+    (has-player-won? :o board)
+    (has-cat-won? board)))
 
 (defn has-player-won-the-multi-board? [symbol multiboard]
   (some? (some
@@ -40,10 +46,7 @@
            lines)))
 
 (defn has-cat-won-the-multi-board? [multiboard]
-  (every? #(or (has-player-won? :x %1)
-            (has-player-won? :o %1)
-            (has-cat-won? %1))
-       multiboard))
+  (every? is-complete? multiboard))
 
 (defn stringify-board [board]
   (apply format
@@ -101,20 +104,6 @@
 (defn clear-screen []
   (print "\033[H\033[2J"))
 
-(defn prompt [msg options]
-  (loop []
-    (print msg "[" (clojure.string/join "," (map inc options)) "]: ") (flush)
-    (let [choice (- (parse-long (read-line)) 1)]
-      (if (some #(= choice %1) options)
-        choice
-        (recur)))))
-
-(defn is-complete? [board]
-  (or
-    (has-player-won? :x board)
-    (has-player-won? :o board)
-    (has-cat-won? board)))
-
 (defn possible-board-choices [multiboard & [last-move]]
   (let [last-square-chosen (last (or last-move [nil]))]
     (filter
@@ -124,16 +113,29 @@
                 (is-complete? (multiboard last-square-chosen))))
       (range 9))))
 
-(defn get-board-choice [multiboard & [last-move]]
-  (prompt "Board" (possible-board-choices multiboard last-move)))
-
 (defn possible-square-choices [board]
   (filter
     #(nil? (board %1))
     (range 9)))
 
+(defn all-possible-moves [multiboard last-move]
+  (apply concat
+    (map (fn [board] (map #(do [board %1]) (possible-square-choices (multiboard board))))
+      (possible-board-choices multiboard last-move))))
+
+(defn prompt [msg options]
+  (loop []
+    (print msg "[" (clojure.string/join "," (map inc options)) "]: ") (flush)
+    (let [choice (- (parse-long (read-line)) 1)]
+      (if (some #(= choice %1) options)
+        choice
+        (recur)))))
+
 (defn get-square-choice [board]
   (prompt "Square" (possible-square-choices board)))
+
+(defn get-board-choice [multiboard & [last-move]]
+  (prompt "Board" (possible-board-choices multiboard last-move)))
 
 (defn human-player [multiboard last-move]
   (clear-screen)
@@ -148,25 +150,6 @@
         square-choice (rand-nth (possible-square-choices (multiboard board-choice)))]
     [board-choice square-choice]))
 
-(defn title-screen []
-  (clear-screen)
-  (println
-    (clojure.string/replace
-      (clojure.string/join "\n" ["           _   _ _ _   _                 _                "
-                                 "          | | | | | |_(_)_ __ ___   __ _| |_ ___          "
-                                 "          | | | | | __| | '_ ` _ ? / _` | __/ _ ?         "
-                                 "          | |_| | | |_| | | | | | | (_| | ||  __/         "
-                                 "           ?___/|_|?__|_|_| |_| |_|?__,_|?__?___|         "
-                                 "      _____ _        _____            _____               "
-                                 "     |_   _(_) ___  |_   _|_ _  ___  |_   _|__   __       "
-                                 "       | | | |/ __|   | |/ _` |/ __|   | |/ _ ? / _ ?     "
-                                 "       | | | | (__    | | (_| | (__    | | (_) |  __/     "
-                                 "       |_| |_|?___|   |_|?__,_|?___|   |_|?___/ ?___|     "
-                                 "                                                          "
-                                 "                 <<Press enter to start>>                 "])
-      "?" "\\"))
-  (read-line))
-
 (defn play-the-game [x o & [multiboard last-move]]
   (loop [multiboard (or multiboard empty-multi-board)
          symbol :x
@@ -178,11 +161,6 @@
       :else (let [prompt-player (if (= :x symbol) x o)
                   next-move (prompt-player multiboard last-move)]
               (recur (assoc-in multiboard next-move symbol) (opponent symbol) next-move)))))
-
-(defn all-possible-moves [multiboard last-move]
-  (apply concat
-    (map (fn [board] (map #(do [board %1]) (possible-square-choices (multiboard board))))
-      (possible-board-choices multiboard last-move))))
 
 (defn simulate-next-move [multiboard next-move]
   ((play-the-game random-bot random-bot
@@ -208,6 +186,25 @@
              (= :x (result :winner)) "X has won!"
              (= :o (result :winner)) "O has won!"
              :else "Best 2 out of 3?")))
+
+(defn title-screen []
+  (clear-screen)
+  (println
+    (clojure.string/replace
+      (clojure.string/join "\n" ["           _   _ _ _   _                 _                "
+                                 "          | | | | | |_(_)_ __ ___   __ _| |_ ___          "
+                                 "          | | | | | __| | '_ ` _ ? / _` | __/ _ ?         "
+                                 "          | |_| | | |_| | | | | | | (_| | ||  __/         "
+                                 "           ?___/|_|?__|_|_| |_| |_|?__,_|?__?___|         "
+                                 "      _____ _        _____            _____               "
+                                 "     |_   _(_) ___  |_   _|_ _  ___  |_   _|__   __       "
+                                 "       | | | |/ __|   | |/ _` |/ __|   | |/ _ ? / _ ?     "
+                                 "       | | | | (__    | | (_| | (__    | | (_) |  __/     "
+                                 "       |_| |_|?___|   |_|?__,_|?___|   |_|?___/ ?___|     "
+                                 "                                                          "
+                                 "                 <<Press enter to start>>                 "])
+      "?" "\\"))
+  (read-line))
 
 (defn -main [& args]
   (title-screen)
